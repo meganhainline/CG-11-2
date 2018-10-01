a512e753d1ee249565e3d2fdbbcc929f4bb5a40c
Megan Hainline <meg.hainline@gmail.com>
2018-09-30 21:08:18 -0500

    final

--- CG 11-2/Program.cs
+++ CG 11-2/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using CG_11_2;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace CG_11_2
+namespace StudentGrade
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StreamReader file = new StreamReader("C:\\Users\\megha\\source\\repos\\CG 11-2\\CG 11-2\\StudentData.txt");
+
+            List<Student> students = new List<Student>();
+
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] items = line.Split(" ");
+                Student s = new Student(items[0]);
+
+                for (int i = 1; i < items.Length; i++)
+                {
+                    s.AddScore(Int32.Parse(items[i]));
+                }
+
+                students.Add(s);
+
+            }
+            foreach (Student item in students)
+            {
+                Console.WriteLine(item.Name + " " + item.Average());
+            }
+            Console.ReadLine();
+
         }
     }
 }
---
+++ CG 11-2/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CG_11_2
+{
+    class Student
+    {
+        public List<int> scores = new List<int>();
+        public string Name { get; set; }
+
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+        public void AddScore(int score)
+        {
+            scores.Add(score);
+        }
+
+        public double Average()
+        {
+            return scores.Average(x => x);
+        }
+    }
+}
+
